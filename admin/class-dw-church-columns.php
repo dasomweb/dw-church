ed9bf839fb1cbeab0e55240d829b4ca42f6f0165
dasomweb <dasomweb@gmail.com>
2025-11-14 21:36:54 -0500

    Fix: Remove quotes from JavaScript comment causing PHP parse error
    - Remove quotes from comment in JavaScript code that was breaking PHP string
    - Fix critical error: syntax error on line 778
    - Update version to 2.64.6

--- admin/class-dw-church-columns.php
+++ admin/class-dw-church-columns.php
@@ -775,7 +775,7 @@ class DW_Church_Columns {
                                 
                                 // Fill WordPress default date fields
                                 if (ajaxData.post_date) {
-                                    // post_date format: "Y-m-d H:i:s" or "Y-m-d"
+                                    // post_date format: Y-m-d H:i:s or Y-m-d
                                     var dateMatch = ajaxData.post_date.match(/(\d{4})-(\d{2})-(\d{2})/);
                                     if (dateMatch) {
                                         $('input[name=\"aa\"]').val(dateMatch[1]);
