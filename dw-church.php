e0d8c21111e6a8567552774fdddc2b669c6546fa
dasomweb <dasomweb@gmail.com>
2025-12-06 00:43:32 -0500

    Bump version to 2.71.0

--- dw-church.php
+++ dw-church.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: DW Church
  * Description: DW Church Management System
- * Version: 2.70.4
+ * Version: 2.71.0
  * Author: DasomWeb
  * Author URI: https://dasomweb.com
  * Plugin URI: https://github.com/dasomweb/dasom-church-management-system
